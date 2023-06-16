5dcc15934e37e8116c9017c1e439a50869346e53
Jhonatta-Matias896 <131813744+Jhonatta-Matias896@users.noreply.github.com>
2023-06-16 00:19:25 -0300

    refatoração-do-teste-de-cadastro

--- cypress/e2e/UI/tela-de-cadastro.cy.js
+++ cypress/e2e/UI/tela-de-cadastro.cy.js
@@ -7,18 +7,18 @@ describe('Teste na tela de cadastro', () => {
     });
     it('Teste 01: Cadastro com sucesso', () => {
         cy.get(elements.btnCadastro).click()
-        cy.get(elements.campoEmailCadastro).type('teste@test.cim', {force:true})
-        cy.get(elements.campoNomeCadastro).type('Aleatorio',{force:true})
-        cy.get(elements.campoSenhaCadastro).type('N/12345', {force:true})
-        cy.get(elements.campoSenhaCadastroConfirmacao).type('N/12345', {force:true})
+        cy.get(elements.campoEmailCadastro).type(Cypress.env('email01'), {force:true})
+        cy.get(elements.campoNomeCadastro).type(Cypress.env('usuario01'),{force:true})
+        cy.get(elements.campoSenhaCadastro).type(Cypress.env('senhaUsuario'), {force:true})
+        cy.get(elements.campoSenhaCadastroConfirmacao).type(Cypress.env('senhaUsuario'), {force:true})
         cy.get(elements.btnCriarContaComSaldo).click({force:true})
         cy.get(elements.btnCadastrar).click({force:true})
-        cy.get('#modalText').invoke('text').then((texto)=>{
+        cy.get(elements.alertaDecontaCriada).invoke('text').then((texto)=>{
             const NumeroDaConta= texto.match(/\d+-\d+/)[0];
-            cy.get('#btnCloseModal').click()
-            cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type('teste@test.cim', {force:true})
-            cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('N/12345', {force:true})
-            cy.get('.otUnI').click()
+            cy.get(elements.btnCloseAlertaDeContaCriada).click()
+            cy.get(elements.campoEmailLogin).type(Cypress.env('email01'), {force:true})
+            cy.get(elements.campoSenhaLogin).type(Cypress.env('senhaUsuario'), {force:true})
+            cy.get(elements.btnAcessar).click()
             cy.contains(NumeroDaConta).should('be.visible')
         }) 
         //TESTE SEM A ASSERTIVA NO MOMENTO.
--- cypress/support/Elements/global-elements.js
+++ cypress/support/Elements/global-elements.js
@@ -7,6 +7,14 @@ export const  ELEMENTS = {
     btnCriarContaComSaldo:'#toggleAddBalance',
     btnCadastrar:'.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0',
     btnCadastro:'.ihdmxA',
+    alertaDecontaCriada:'#modalText',
+    btnCloseAlertaDeContaCriada:'#btnCloseModal',
+
+    //ELEMENTOS REFERENTES A TELA DE LOGIN
+    campoEmailLogin:'.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default',
+    campoSenhaLogin:'.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default',
+    btnAcessar:'.otUnI',
+
 
 
     
