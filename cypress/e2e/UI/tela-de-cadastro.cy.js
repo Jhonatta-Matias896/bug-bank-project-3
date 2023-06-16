3d6d9267c72bae955e2329f9ded85a22fe146cb5
Jhonatta-Matias896 <131813744+Jhonatta-Matias896@users.noreply.github.com>
2023-06-16 00:05:06 -0300

    correção-da-assertiva-teste-01

--- cypress/e2e/UI/tela-de-cadastro.cy.js
+++ cypress/e2e/UI/tela-de-cadastro.cy.js
@@ -14,9 +14,12 @@ describe('Teste na tela de cadastro', () => {
         cy.get(elements.btnCriarContaComSaldo).click({force:true})
         cy.get(elements.btnCadastrar).click({force:true})
         cy.get('#modalText').invoke('text').then((texto)=>{
-            const NumeroDaConta= texto.match(/\d+-\d+/)[0]
-            cy.contains(NumeroDaConta)
-            cy.get('#btnCloseModal').click()  
+            const NumeroDaConta= texto.match(/\d+-\d+/)[0];
+            cy.get('#btnCloseModal').click()
+            cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type('teste@test.cim', {force:true})
+            cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('N/12345', {force:true})
+            cy.get('.otUnI').click()
+            cy.contains(NumeroDaConta).should('be.visible')
         }) 
         //TESTE SEM A ASSERTIVA NO MOMENTO.
         // cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type('teste@test.cim', {force:true})
